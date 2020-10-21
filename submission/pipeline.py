d1681e20615c359f6f1f9b34ffb90c37baea82da
Rajendra Singh <rajendra.singh@oneconvergence.com>
2020-10-21 13:15:40 -0500

    made dataset as argument

--- submission/pipeline.py
+++ submission/pipeline.py
@@ -8,13 +8,11 @@ from kubernetes.client.models import V1EnvVar
     name="Titanic Experiment pipeline",
     description="A pipline showing how to use evaluation component",
 )
-def titanic_pipline(token, project_id):
+def titanic_pipline(token, project_id, dataset, version):
     pipelineConfig = kfp.dsl.PipelineConf()
     pipelineConfig.set_image_pull_policy("Always")
 
-    input_volumes = json.dumps(
-        ["titanic-test-pvc@dataset://titanic-test/1603048660622"]
-    )
+    input_volumes = json.dumps([f"{dataset}-pvc@dataset://{dataset}/{version}"])
     storage_op = kfp.dsl.ContainerOp(
         "get_dataset",
         image="ocdr/dkubepl:storage_v1",
@@ -38,7 +36,7 @@ def titanic_pipline(token, project_id):
         "predict",
         image="ocdr/titanic_submission",
         command=["python", "predict.py"],
-        pvolumes={"/titanic-test/": kfp.dsl.PipelineVolume(pvc="titanic-test-pvc")},
+        pvolumes={"/titanic-test/": kfp.dsl.PipelineVolume(pvc=f"{dataset}-pvc")},
         file_outputs={"output": "/tmp/prediction.csv"},
     )
     predict_op.after(storage_op)
@@ -65,6 +63,10 @@ def titanic_pipline(token, project_id):
 
 if __name__ == "__main__":
     token = "eyJhbGciOiJSUzI1NiIsImtpZCI6Ijc0YmNkZjBmZWJmNDRiOGRhZGQxZWIyOGM2MjhkYWYxIn0.eyJ1c2VybmFtZSI6Im9jIiwicm9sZSI6ImRhdGFzY2llbnRpc3QsbWxlLHBlLG9wZXJhdG9yIiwiZXhwIjo0ODQyODUzNzk0LCJpYXQiOjE2MDI4NTM3OTQsImlzcyI6IkRLdWJlIn0.3LFnWC1fI2vUVn2EHIaodC_BJxv2G19PbdyLVx8Vzid5CI_9F_UnAjX9N3P1cgO-npf4FnQqYM3hMGyRh1JCnNA2Ag2rXx4g5Da4ecdsndhZjXwtuEulXAGJO2FGe_L5zCciWEzOZB9JUW1xdOyLOZOUAT71TxBnX7rVRvQHyuTY3SF1Fs7na5z82ggMgFC2BI1vhc2XCwV9u6ftqww3rogdPMLri7mbtyQIQ9Ip-NrOOcjXyFD5Uow7-PevnkxLSOICAh7QvvI7WHxJxa6xDHuokPaX-q8qk1tHm4-hbFUcyIa8WeUB-DDxyWgczQmQ_s70q4cJt2vwPbRAfoyWqw"
-    kfp.Client().create_run_from_pipeline_func(
-        titanic_pipline, arguments={"token": token, "project_id": "ucpu3w"}
-    )
+    args = {
+        "token": token,
+        "project_id": "ucpu3w",
+        "dataset": "titanic-test",
+        "version": "1603048660622",
+    }
+    kfp.Client().create_run_from_pipeline_func(titanic_pipline, arguments=args)
