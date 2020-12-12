1af51318af8d03a0b131bcad9497d62832b09e25
Rajendra <rajendra.singh@oneconvergence.com>
2020-12-11 18:56:52 -0600

    updated predict script

--- predict.py
+++ predict.py
@@ -4,13 +4,14 @@ import click
 
 import numpy as np
 import pandas as pd
-
+from tensorflow import keras
 
 @click.command()
 def predict():
-    os.system("ls -l /titanic-test/")
 
-    df = pd.read_csv("/titanic-test/test.csv")
+    df = pd.read_csv("/dataset/test.csv")
+    keras.models.load_model("/model/model.h5")
+    
     predictions = np.random.choice([0, 1], size=(len(df),), p=[1.0 / 3, 2.0 / 3])
     df["Survived"] = predictions
     df = df.set_index("PassengerId")
@@ -24,3 +25,4 @@ def predict():
 
 if __name__ == "__main__":
     predict()
+
