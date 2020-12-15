069262df5a47c3c6ccc1a6af5305822ee31150b3
riteshkarvaloc <ritesh.k@oneconvergence.com>
2020-12-15 23:15:50 +0530

    fs

--- predict.py
+++ predict.py
@@ -1,28 +1,18 @@
 import os
-
-import click
-
+import joblib
 import numpy as np
 import pandas as pd
-from tensorflow import keras
 
-@click.command()
+model_dir = "/model"
+
 def predict():
-    os.system("ls -l /titanic-test/ /model/")
-    df = pd.read_csv("/titanic-test/test.csv")
-    keras.models.load_model("/model/model.h5")
-    
-    predictions = np.random.choice([0, 1], size=(len(df),), p=[1.0 / 3, 2.0 / 3])
-    df["Survived"] = predictions
-    df = df.set_index("PassengerId")
-    df.to_csv(
-        "/tmp/prediction.csv",
-        index=True,
-        columns=["Survived"],
-    )
+    test_df = pd.read_csv(os.path.join(model_dir, "test.csv"))
+    model = joblib.load(os.path.join(model_dir, "model.joblib"))
+    predictions = model.predict(test_df)
+    output = pd.DataFrame({'PassengerId': test_df.PassengerId, 'Survived': predictions})
+    output.to_csv('my_submission.csv', index=False)
     print("predictions generated.")
 
 
 if __name__ == "__main__":
     predict()
-
