9c93bf805d04190a2adf230a775d79ef5b1be484
riteshkarvaloc <ritesh.k@oneconvergence.com>
2020-12-16 17:35:45 +0530

    fs

--- predict.py
+++ predict.py
@@ -7,10 +7,15 @@ model_dir = "/model"
 
 def predict():
     test_df = pd.read_csv(os.path.join(model_dir, "test.csv"))
+    test_df = pd.DataFrame(test_df).fillna(test_df.mean())
     model = joblib.load(os.path.join(model_dir, "model.joblib"))
     predictions = model.predict(test_df)
-    output = pd.DataFrame({'PassengerId': test_df.PassengerId, 'Survived': predictions})
-    output.to_csv('my_submission.csv', index=False)
+    df = pd.DataFrame({'PassengerId': test_df.index, 'Survived': predictions})
+    df.to_csv(
+        "/tmp/prediction.csv",
+        index=True,
+        columns=["Survived"],
+    )
     print("predictions generated.")
 
 
